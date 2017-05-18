0b7ce99921397e76b387c8c5789de0b34cb2ac58
YETC7 <tyler_726@live.com>
2017-05-17 23:39:54 -0700

    display random target score and new score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,57 @@
+var crystal0 = {
+	value: 7
+};
+
+var crystal1 = {
+	value: 2
+};
+
+var crystal2 = {
+	value: 6
+};
+
+var crystal3 = {
+	value: 3
+};
+
 var targetScore;
-var userScore = 0;
+
+var userScore;
+
+
+startGame();
+
+function startGame() {
+	targetScore = Math.floor(Math.random()*102 + 19);
+	$("#targetScore").text(targetScore);
+
+	userScore = 0;
+	$("#userScore").text(userScore);
+}
+
+
+
+
 
 
 $(".imgContainer").on("click",function() {
 	// console.log(this.id);
 	if(this.id === "crystal0") {
-		userScore+=1;
+		userScore+=crystal0.value;
 	}
 
 	else if(this.id === "crystal1") {
-		userScore+=2;
+		userScore+=crystal1.value;
 	}
 
 	else if(this.id === "crystal2") {
-		userScore+=3;
+		userScore+=crystal2.value;
 	}
 
 	else if(this.id === "crystal3") {
-		userScore+=4;
+		userScore+=crystal3.value;
 	}
 
 	console.log(userScore);
+	$("#userScore").text(userScore);
 });
