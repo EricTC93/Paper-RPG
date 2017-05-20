7dcc7619ad5374a86032426ea09ebf4d3ed9b6f7
YETC7 <tyler_726@live.com>
2017-05-19 21:57:40 -0700

    user selects character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,59 @@
+// Declaring Variables
 var mario = {
 	name:"Mario",
 	img: "assets/images/mario.png",
-	$container: $("#char0")
+	imgAlt: "assets/images/mario.png",
+	$container: $("#char0"),
+	id: $("#char0").attr("id")
 };
 
 var luigi = {
 	name:"Luigi",
 	img: "assets/images/luigi.png",
-	$container: $("#char1")
+	imgAlt: "assets/images/luigiAlt.png",
+	$container: $("#char1"),
+	id: $("#char1").attr("id")
 };
 
 var peach = {
 	name:"Peach",
 	img: "assets/images/peach.png",
-	$container: $("#char2")
+	imgAlt: "assets/images/peachAlt.png",
+	$container: $("#char2"),
+	id: $("#char2").attr("id")
 };
 
 var bowser = {
 	name:"Bowser",
 	img: "assets/images/bowser.png",
-	$container: $("#char3")
+	imgAlt: "assets/images/bowser.png",
+	$container: $("#char3"),
+	id: $("#char3").attr("id")
 };
+
+var characters = [mario,luigi,peach,bowser];
 
 // console.log(mario);
 
-$("#selectRow").append(mario.$container);
+// $("#selectRow").append(mario.$container);
+
+
+var playerSelected = false;
+var userPlayer
+
+$(".characterContainer").on("click",function() {
+	// console.log(this.id);
+	// console.log(characters[0].id);
+	if (!playerSelected) {
+
+		playerSelected = true;
+		for (var i = 0; i < characters.length; i++) {
+
+			if (this.id != characters[i].id) {
+				$("#selectRow").append(characters[i].$container);
+
+			}
+		}
+	}
+});
+
